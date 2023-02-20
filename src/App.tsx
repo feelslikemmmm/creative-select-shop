66dcf4a33475e28623fde18937c32bb3acb998e2
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2023-02-21 02:27:53 +0900

    feat: router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Navbar from '@components/Navbar';
+import { Outlet } from 'react-router-dom';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
