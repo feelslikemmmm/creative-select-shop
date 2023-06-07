ba8f1dd446029dc1395029cd425debcbfa71056c
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2023-06-07 23:58:06 +0900

    feat: button 컴포넌트 disabled 추가

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 interface ButtonProps {
   text: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
 }
 
-export default function Button({ text, onClick }: ButtonProps) {
+export default function Button({ text, onClick, disabled }: ButtonProps) {
   return (
     <button
       className="bg-brand text-white py-2 px-4 rounded-sm hover:brightness-110"
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
