85f14f24e78157c726bc7b1a64dff5e9670c200a
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2023-06-07 23:56:49 +0900

    feat: queryClient setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import Navbar from '@components/Navbar';
 import { AuthContexntProvider } from '@components/context/AuthContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Outlet } from 'react-router-dom';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <AuthContexntProvider>
         <Navbar />
         <Outlet />
       </AuthContexntProvider>
-    </>
+    </QueryClientProvider>
   );
 }
 
