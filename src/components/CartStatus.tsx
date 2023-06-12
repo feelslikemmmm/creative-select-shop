5e36dd10bf3e2c2239be9a68c60476bba576e157
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2023-06-13 03:54:51 +0900

    feat: cartStatus update

--- src/components/CartStatus.tsx
+++ src/components/CartStatus.tsx
@@ -8,6 +8,7 @@ export default function CartStatus() {
 
   return (
     <div className="relative">
+      {/* <AiOutlineShoppingCart className="text-4xl" /> */}
       <AiOutlineShoppingCart className="text-4xl" />
       {products && (
         <p className="w-6 h-6 text-center bg-brand text-white font-bold rounded-full absolute -top-1 -right-2">
