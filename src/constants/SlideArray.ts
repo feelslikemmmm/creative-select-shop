3e5d7a210db572d0540972a0436c9fff8b2ac8d4
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2023-06-16 06:20:54 +0900

    feat: slide 상수 추가

---
+++ src/constants/SlideArray.ts
@@ -0,0 +1,14 @@
+export const slideArray = [
+  {
+    img: process.env.PUBLIC_URL + '/images/slide1.png',
+    alt: 'slide1',
+  },
+  {
+    img: process.env.PUBLIC_URL + '/images/slide2.png',
+    alt: 'slide2',
+  },
+  {
+    img: process.env.PUBLIC_URL + '/images/slide3.png',
+    alt: 'slide3',
+  },
+];
